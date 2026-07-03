b0ed4e149c478a6ec42a12c310767a4a1e8dfea8
Sognefjellet <140601728+sognefjellet@users.noreply.github.com>
2026-07-03 08:22:18 +0200

    #36 Prevent -SNAPSHOT suffix to release builds and replaced 'echo -n' with 'printf "%s"' because some shell implementation like sh don't support the -n argument.

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -5,17 +5,22 @@ set -o nounset
 # Usage: set-version.sh <branch-name> <version-suffix>
 
 BRANCH_NAME="${1:-$(git branch | grep -E '^\* ' | sed 's|\* ||')}"
-export BRANCH_NAME="$(echo -n "${BRANCH_NAME}" | sed 's|.*/||')"
+export BRANCH_NAME="$(printf '%s' "${BRANCH_NAME}" | sed 's|.*/||')"
 
-VERSION_SUFFIX="${2:--SNAPSHOT}"
-export VERSION_NUMBER="$(echo -n "${BRANCH_NAME}" | sed 's|^v\(ersion\)\{0,1\}||ig')${VERSION_SUFFIX}"
+if [ "$#" -gt 1 ]; then
+  VERSION_SUFFIX="${2:-}"
+else
+  VERSION_SUFFIX="-SNAPSHOT"
+fi
+
+export VERSION_NUMBER="$(printf '%s' "${BRANCH_NAME}" | sed 's|^v\(ersion\)\{0,1\}||ig')${VERSION_SUFFIX}"
 IFS=. read VERSION_NUMBER_MAJOR VERSION_NUMBER_MINOR VERSION_NUMBER_BUGFIXNR <<<"$(echo "${VERSION_NUMBER}" | grep -o -E '^\d+\.\d+\.\d+' || cat version.txt)"
 export VERSION_NUMBER_MAJOR
 export VERSION_NUMBER_MINOR
 export VERSION_NUMBER_BUGFIXNR
 
 echo "Setting version to ${VERSION_NUMBER} (${VERSION_NUMBER_MAJOR}.${VERSION_NUMBER_MINOR}.${VERSION_NUMBER_BUGFIXNR}) from branch ${BRANCH_NAME}."
-echo -n "${VERSION_NUMBER_MAJOR}.${VERSION_NUMBER_MINOR}.${VERSION_NUMBER_BUGFIXNR}" >version.txt
+printf '%s' "${VERSION_NUMBER_MAJOR}.${VERSION_NUMBER_MINOR}.${VERSION_NUMBER_BUGFIXNR}" >version.txt
 mvn -B versions:set \
 	-DnewVersion="${VERSION_NUMBER}" \
 	-DoldVersion=0.0.0 \
